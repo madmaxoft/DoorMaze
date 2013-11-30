04a718ef01c04a99a23d1b01ba922387b9bbded8
madmaxoft <github@xoft.cz>
2013-11-30 22:21:37 +0100

    The maze now changes shape.

--- DoorMaze.lua
+++ DoorMaze.lua
@@ -15,10 +15,67 @@ local MAZE_HEIGHT = 5;
 
 
 
+--- Toggles a few doors in a single maze
+function ToggleMazeDoors(a_Maze)
+	for i = 0, 15 do
+		local rndx = math.random(2 * MAZE_SIZE);
+		local rndz = math.random(2 * MAZE_SIZE);
+		local Height = a_Maze.Height[rndz][rndx];
+		if (Height >= 0) then
+			local BlockX = a_Maze.Center.x + rndx - MAZE_SIZE;
+			local BlockZ = a_Maze.Center.z + rndz - MAZE_SIZE;
+
+			-- TODO: We have no cWorld:ToggleDoor() API, we need to do it by hand:
+			local BlockValid, BlockType, BlockMeta = a_Maze.World:GetBlockTypeMeta(BlockX, Height + 2, BlockZ);
+			if (BlockValid and (BlockType == E_BLOCK_WOODEN_DOOR)) then
+				if (BlockMeta == 8) then
+					BlockMeta = 9;
+				else
+					BlockMeta = 8;
+				end
+				a_Maze.World:SetBlockMeta(BlockX, Height + 2, BlockZ, BlockMeta);
+			end
+		end
+	end
+end
+
+
+
+
 
 function OnWorldTick(a_World, a_Dt, a_LastTickDuration)
 	-- TODO: Toggle some of mazes' doors
-	-- for idx, maze in ipairs(g_Mazes)
+	for idx, maze in ipairs(g_Mazes) do
+		if (maze.World == a_World) then
+			-- Check if it's late enough after the last toggle:
+			if (maze.NumTicksLeft > 0) then
+				maze.NumTicksLeft = maze.NumTicksLeft - 1;
+			else
+				maze.NumTicksLeft = a_LastTickDuration / 2;  -- The more msec per tick, the slower the doors are changed
+				
+				-- Check if there's a player in or near the maze, toggle doors only if so:
+				local IsActive = false;
+				maze.World:ForEachPlayer(
+					function (a_Player)
+						local PlayerPos = a_Player:GetPosition();
+						if (
+							(PlayerPos.x >= maze.Center.x - 2 * MAZE_SIZE) and
+							(PlayerPos.x <= maze.Center.x + 2 * MAZE_SIZE) and
+							(PlayerPos.z >= maze.Center.z - 2 * MAZE_SIZE) and
+							(PlayerPos.z <= maze.Center.z + 2 * MAZE_SIZE)
+						) then
+							IsActive = true;
+							return true;
+						end
+					end
+				);
+				
+				if (IsActive) then
+					ToggleMazeDoors(maze);
+				end
+			end
+		end
+	end  -- for maze - g_Mazes[]
 end
 
 
@@ -31,14 +88,14 @@ function FindMazeHeight(a_WorldImage, a_BlockX, a_BlockZ, a_CenterY)
 		if (
 			(a_WorldImage:GetBlockType(a_BlockX, a_CenterY + y, a_BlockZ) == E_BLOCK_AIR) and
 			(a_WorldImage:GetBlockType(a_BlockX, a_CenterY + y + 1, a_BlockZ) == E_BLOCK_AIR) and
-			(a_WorldImage:GetBlockType(a_BlockX, a_CenterY + y - 1, a_BlockZ) ~= E_BLOCK_AIR)
+			g_BlockIsSolid[a_WorldImage:GetBlockType(a_BlockX, a_CenterY + y - 1, a_BlockZ)]
 		) then
 			return y + MAZE_HEIGHT;
 		end
 		if (
 			(a_WorldImage:GetBlockType(a_BlockX, a_CenterY - y, a_BlockZ) == E_BLOCK_AIR) and
 			(a_WorldImage:GetBlockType(a_BlockX, a_CenterY - y + 1, a_BlockZ) == E_BLOCK_AIR) and
-			(a_WorldImage:GetBlockType(a_BlockX, a_CenterY - y - 1, a_BlockZ) ~= E_BLOCK_AIR)
+			g_BlockIsSolid[a_WorldImage:GetBlockType(a_BlockX, a_CenterY - y - 1, a_BlockZ)]
 		) then
 			return MAZE_HEIGHT - y;
 		end
@@ -57,6 +114,7 @@ function GenerateMaze(a_World, a_MazeCenter)
 	a_MazeCenter.z = math.floor(a_MazeCenter.z);
 	Maze.Center = a_MazeCenter;
 	Maze.World = a_World;
+	Maze.NumTicksLeft = 50;
 	
 	local CenterY = math.floor(a_MazeCenter.y);
 	local MinX = a_MazeCenter.x - MAZE_SIZE;
